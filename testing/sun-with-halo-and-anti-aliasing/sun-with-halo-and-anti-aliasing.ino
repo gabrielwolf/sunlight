82cb082d7a0f26d31abea81d9e9f075d378b342a
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2017-09-09 01:30:38 +0200

    The actual sun's position is now mapped to the LED strip (24h)

--- testing/sun-with-halo-and-anti-aliasing/sun-with-halo-and-anti-aliasing.ino
+++ testing/sun-with-halo-and-anti-aliasing/sun-with-halo-and-anti-aliasing.ino
@@ -1,4 +1,5 @@
 // Sunlight with halo and anti aliasing
+// The actual sun's position is mapped to the LED strip (24h)
 
 // Video at: https://vimeo.com/wolfzeitlos/sun-with-halo-and-anti-aliasing
 // Git at: https://github.com/gabrielwolf/sunlight
@@ -12,13 +13,17 @@
 #define NUM_LEDS 144
 #define BRIGHTNESS 255
 
+#include <TimeLib.h>
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+
 const int potPin = A0;  // the purpose of the poti is testing variables tactically
 int readValue;
 double subpixel;
 
 double sunSize = NUM_LEDS * 0.3;      // set the sun's size as a percentage of the total number of pixels
 double sunHaloSize = sunSize * 1.5;   // define the halo's size in relation to the sun's size
-double sunBrightness = 200;
+double sunBrightness = 180;
 double haloBrightness = sunBrightness * 1 / 4;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
@@ -49,17 +54,66 @@ void setup() {
   strip.setBrightness(BRIGHTNESS);
   strip.begin();
   strip.show();                       // initialize all pixels to 'off'
+  setSyncProvider(requestSync);       //set function to call when sync required
+  Serial.println("Waiting for sync message");
 }
 
 
 void loop() {
+  // to initialize the Arduino with a correct time type the following in your console:
+  // $ TZ_adjust=+2; // to correct your time zone
+  // $ sudo echo "T$(($(date +%s)+60*60*$TZ_adjust))" >/dev/tty.usbmodemfa131
+
+  // if it doesn't do, just type
+  //
+  // $ TZ_adjust=-5;
+  // $ echo T$(($(date +%s)+60*60*$TZ_adjust))
+  //
+  // to get a correct UNIX time, that you can enter in the serial monitor, after the arduino has been started
+
+  if (Serial.available()) {
+    processSyncMessage();
+  }
+  if (timeStatus()!= timeNotSet) {
+    digitalClockDisplay();  
+  }
+
   // read poti in 10 bit
-  readValue = analogRead(potPin);
+  //  int poti = analogRead(potPin);
 
-  // map position of the poti to the number of pixels, and add imaginary Pixels left and right.
-  // now we can partially see the sun
-  subpixel = mapdouble(readValue, 0, 1023, - sunHaloSize, NUM_LEDS - 1 + sunHaloSize);
+  // calc all seconds that are gone by now for today  
+  time_t t = now();
+  double hours = hour(t); // map(poti, 0, 1023, 0, 23);
+  double minutes = minute(t);
+  double seconds = second(t);
+  double secondsGoneToday = hours * 3600 + minutes * 60 + seconds;
 
+  // same thing in milliseconds for the actial second
+  int actualMilliSecs = millis() % 1000;
+  double millisPerSec = 1000;
+
+  // imaginary pixels are going to be used for twilight, when we have sunset and sunrise time values
+  // subpixel = mapdouble(secondsGoneToday+1*actualMilliSecs/millisPerSec, 0, 86400, -sunHaloSize, NUM_LEDS - 1 +sunHaloSize);
+
+  subpixel = mapdouble(secondsGoneToday+1*actualMilliSecs/millisPerSec, 0, 86400, 0, NUM_LEDS - 1);
+
+  /*
+  Serial.print("Sunsize: ");
+  Serial.println(sunSize);
+
+  Serial.print("SunHalosize: ");
+  Serial.println(sunHaloSize);
+
+  Serial.print("Subpixl: ");
+  Serial.println(subpixel);
+
+  Serial.print("secondsGoneToday: ");
+  Serial.println(secondsGoneToday);
+
+  Serial.print("secondsGoneToday+1*actualMilliSecs/millisPerSec: ");
+  Serial.print(secondsGoneToday+1*actualMilliSecs/millisPerSec);
+  */
+  
   // wipe values of all pixels to start every loop a clean calculating
   strip.clear();
 
@@ -67,15 +121,11 @@ void loop() {
   double from = subpixel - sunHaloSize / 2;
   double to = subpixel + sunHaloSize / 2;
 
-  // debug via serial monitoring
-  // Serial.println(from);
-
   // Halo
   for (int i = from + 1; i <= to; i++) {
     double cosinus = mapdouble(i, from, to, -PI / 2, PI / 2);
     strip.setPixelColor(i, haloBrightness * abs(cos(cosinus)), 0, 0, 0);
   }
-
 
   // set the actual position and width of the sun
   from = subpixel - sunSize / 2;
@@ -89,7 +139,7 @@ void loop() {
     uint8_t b = blue(color);
     uint8_t w = white(color);
     double cosinus = mapdouble(i, from, to, -PI / 2, PI / 2);
-    strip.setPixelColor(i, r + sunBrightness * pow(abs(cos(cosinus)), 3), 0, 0, 0);   // cosine is pimped, to sharpen the edge of the sun
+    strip.setPixelColor(i, min(r + sunBrightness * pow(abs(cos(cosinus)), 3), 255), 0, 0, 0);   // cosine is pimped, to sharpen the edge of the sun
   }
 
   // add gamma to all pixels
@@ -126,9 +176,48 @@ uint8_t blue(uint32_t c) {
   return (c);
 }
 
-// mapping in double precision
+// custom mapping in double precision
 double mapdouble(double x, double in_min, double in_max, double out_min, double out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+void processSyncMessage() {
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
+
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+       setTime(pctime); // Sync Arduino clock to the time received on the serial port
+     }
+  }
+}
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+time_t requestSync()
+{
+  Serial.write(TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial mesg
+}
